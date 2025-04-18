043c3f48814bb2237680e39c61c9d27242a4ecd5
chacha86 <taejincha0619@gmail.com>
2025-04-18 13:33:35 +0900

    work

--- frontend/src/lib/backend/client.tsx
+++ frontend/src/lib/backend/client.tsx
@@ -2,11 +2,11 @@ import createClient from "openapi-fetch";
 import { paths } from "./apiV1/schema";
 
 const clientWithNoHeaders = createClient<paths>({
-  baseUrl: "http://localhost:8080",
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 const client = createClient<paths>({
-  baseUrl: "http://localhost:8080",
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
